f4e81f08aeae0882b5731409752c53103dd0d687
Rafał Mikołajun <rafal@mikoweb.pl>
2015-01-03 01:06:31 +0000

    model doctrine

---
+++ Model/DoctrineModelAbstract.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the vSymfo package.
+ *
+ * website: www.vision-web.pl
+ * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace vSymfo\Core\Entity;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Model używający Doctrine
+ * @author Rafał Mikołajun <rafal@vision-web.pl>
+ * @package vSymfo Core
+ * @subpackage Model
+ */
+abstract class DoctrineModelAbstract implements DoctrineModelInterface
+{
+    /**
+     * @var Registry
+     */
+    private $doctrine;
+
+    /**
+     * @param Registry $doctrine
+     */
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @return Registry
+     */
+    public function getDoctrine()
+    {
+        return $this->doctrine;
+    }
+
+    /**
+     * @return EntityManager;
+     */
+    public function getEntityManager()
+    {
+        return $this->doctrine->getEntityManager();
+    }
+}
---
+++ Model/DoctrineModelInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the vSymfo package.
+ *
+ * website: www.vision-web.pl
+ * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace vSymfo\Core\Entity;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @author Rafał Mikołajun <rafal@vision-web.pl>
+ * @package vSymfo Core
+ * @subpackage Model
+ */
+interface DoctrineModelInterface
+{
+    /**
+     * @return Registry
+     */
+    public function getDoctrine();
+
+    /**
+     * @return EntityManager;
+     */
+    public function getEntityManager();
+
+    /**
+     * @return EntityRepository
+     */
+    public function getModelRepository();
+}
