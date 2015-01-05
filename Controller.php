666052d2e01b12878e477cb987d0b737a8d0f0ce
Rafał Mikołajun <rafal@mikoweb.pl>
2015-01-05 20:17:17 +0000

    ułatwienie do tworzenia odpowiedzi z wyłączoną dyrektywą cache

--- Controller.php
+++ Controller.php
@@ -14,6 +14,7 @@ namespace vSymfo\Core;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as SymfonyController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use vSymfo\Component\Document\Format\DocumentAbstract;
 
 /**
@@ -44,6 +45,21 @@ class Controller extends SymfonyController
         $response = $this->render($view, $parameters, $response);
         $this->container->get('document')->body($response->getContent());
         $response->setContent($this->container->get('document')->render());
+
+        return $response;
+    }
+
+    /**
+     * Odpowiedź z wyłączoną dyrektywą cache
+     * @param Response|RedirectResponse $response
+     * @return Response|RedirectResponse
+     */
+    public function noCacheResponse($response)
+    {   $response->headers->addCacheControlDirective('no-cache', true);
+        $response->headers->addCacheControlDirective('max-age', 0);
+        $response->headers->addCacheControlDirective('must-revalidate', true);
+        $response->headers->addCacheControlDirective('no-store', true);
+
         return $response;
     }
 }
