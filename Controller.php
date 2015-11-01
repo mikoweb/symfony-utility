09d1e3504e717d6b5fc1544b6ffc5c29456718d2
Rafał Mikołajun <rafal@mikoweb.pl>
2015-11-01 23:21:57 +0100

    chcę mieć możliwość renderowania dokumentu w kontrolerach nie dziedziczących pod vSymfo\Core\Controller poprzez importowanie traitsa

--- Controller.php
+++ Controller.php
@@ -15,7 +15,7 @@ namespace vSymfo\Core;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as SymfonyController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use vSymfo\Component\Document\Format\DocumentAbstract;
+use vSymfo\Core\Traits\DocumentableControllerTrait;
 
 /**
  * @author Rafał Mikołajun <rafal@vision-web.pl>
@@ -23,31 +23,7 @@ use vSymfo\Component\Document\Format\DocumentAbstract;
  */
 class Controller extends SymfonyController
 {
-    /**
-     * @return DocumentAbstract
-     */
-    public function getDocument()
-    {
-        return $this->container->get('document');
-    }
-
-    /**
-     * Renderowanie widoku z zastosowaniem usługi dokumentu
-     *
-     * @param string   $view       The view name
-     * @param array    $parameters An array of parameters to pass to the view
-     * @param Response $response   A response instance
-     *
-     * @return Response A Response instance
-     */
-    public function renderDocument($view, array $parameters = array(), Response $response = null)
-    {
-        $response = $this->render($view, $parameters, $response);
-        $this->container->get('document')->body($response->getContent());
-        $response->setContent($this->container->get('document')->render());
-
-        return $response;
-    }
+    use DocumentableControllerTrait;
 
     /**
      * Odpowiedź z wyłączoną dyrektywą cache
---
+++ Traits/DocumentableControllerTrait.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the vSymfo package.
+ *
+ * website: www.vision-web.pl
+ * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace vSymfo\Core\Traits;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Response;
+use vSymfo\Component\Document\Format\DocumentAbstract;
+
+/**
+ * Kontroler posiadający renderowanie dokumentu
+ * @author Rafał Mikołajun <rafal@vision-web.pl>
+ * @package vSymfo Core
+ * @subpackage Traits
+ */
+trait DocumentableControllerTrait
+{
+    /**
+     * @return DocumentAbstract
+     */
+    public function getDocument()
+    {
+        return $this->container->get('document');
+    }
+
+    /**
+     * Renderowanie widoku z zastosowaniem usługi dokumentu
+     *
+     * @param string   $view       The view name
+     * @param array    $parameters An array of parameters to pass to the view
+     * @param Response $response   A response instance
+     *
+     * @return Response A Response instance
+     */
+    public function renderDocument($view, array $parameters = array(), Response $response = null)
+    {
+        $response = $this->render($view, $parameters, $response);
+        $this->container->get('document')->body($response->getContent());
+        $response->setContent($this->container->get('document')->render());
+
+        return $response;
+    }
+}
