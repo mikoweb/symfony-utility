f38d46eaa1e755de2b18dfa4a7c4c1ad92f8c4f2
Rafał Mikołajun <rafal@mikoweb.pl>
2015-11-16 22:51:06 +0100

    manager w kontrolerze

---
+++ Manager/ControllerManagerAbstract.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is part of the vSymfo package.
+ *
+ * website: www.vision-web.pl
+ * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace vSymfo\Core\Manager;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\FormFactory;
+
+/**
+ * @author Rafał Mikołajun <rafal@vision-web.pl>
+ * @package vSymfo Core
+ * @subpackage Manager
+ */
+abstract class ControllerManagerAbstract implements ControllerManagerIterface
+{
+    /**
+     * @var EntityManager
+     */
+    private $manager;
+
+    /**
+     * @var FormFactory
+     */
+    private $formFactory;
+
+    /**
+     * @param EntityManager $manager
+     * @param FormFactory $formFactory
+     */
+    public function __construct(EntityManager $manager, FormFactory $formFactory)
+    {
+        $this->manager = $manager;
+        $this->formFactory = $formFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm($data = null, array $options = [])
+    {
+        $form = $this->formFactory
+            ->createBuilder($this->formType(), $data, $options)
+            ->getForm()
+        ;
+
+        return $form;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save($data)
+    {
+        $this->invalidEntityException($data);
+        $this->manager->persist($data);
+        $this->manager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($data)
+    {
+        $this->invalidEntityException($data);
+        $this->manager->remove($data);
+        $this->manager->flush();
+    }
+
+    /**
+     * @param mixed $data
+     *
+     * @throws \UnexpectedValueException
+     */
+    protected function invalidEntityException($data)
+    {
+        if (!$this->isRightEntity($data)) {
+            throw new \InvalidArgumentException();
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract protected function formType();
+}
---
+++ Manager/ControllerManagerIterface.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the vSymfo package.
+ *
+ * website: www.vision-web.pl
+ * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace vSymfo\Core\Manager;
+
+use Symfony\Component\Form\Form;
+
+/**
+ * @author Rafał Mikołajun <rafal@vision-web.pl>
+ * @package vSymfo Core
+ * @subpackage Manager
+ */
+interface ControllerManagerIterface
+{
+    /**
+     * @return mixed
+     */
+    public function createEntity();
+
+    /**
+     * @param $data = null
+     * @param array $options
+     *
+     * @return Form
+     */
+    public function buildForm($data = null, array $options = []);
+
+    /**
+     * @param $data = null
+     *
+     * @return Form
+     */
+    public function buildFormForNew($data = null);
+
+    /**
+     * @param $data
+     *
+     * @return Form
+     */
+    public function buildFormForEdit($data);
+
+    /**
+     * @param mixed $entity
+     *
+     * @return void
+     */
+    public function save($entity);
+
+    /**
+     * @param mixed $entity
+     *
+     * @return void
+     */
+    public function remove($entity);
+
+    /**
+     * @param mixed $entity
+     *
+     * @return boolean
+     */
+    public function isRightEntity($entity);
+}
