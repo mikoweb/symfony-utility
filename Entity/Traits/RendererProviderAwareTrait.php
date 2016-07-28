eb9344343a457abaf9e7710ae781b5c97522d34a
Rafał Mikołajun <rafal@mikoweb.pl>
2016-07-28 19:44:19 +0200

    renderer provider aware

---
+++ Entity/Traits/RendererProviderAwareTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the vSymfo package.
+ *
+ * website: www.vision-web.pl
+ * (c) Rafał Mikołajun <rafal@vision-web.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace vSymfo\Core\Entity\Traits;
+
+use vSymfo\Core\Entity\Provider\RendererProviderInterface;
+
+/**
+ * @author Rafał Mikołajun <rafal@vision-web.pl>
+ * @package vSymfo Core
+ * @subpackage Entity
+ */
+trait UrlProviderAwareTrait
+{
+    /**
+     * @var RendererProviderInterface
+     */
+    protected $rendererProvider;
+
+    /**
+     * @param RendererProviderInterface $rendererProvider
+     */
+    public function setRendererProvider(RendererProviderInterface $rendererProvider)
+    {
+        $this->rendererProvider = $rendererProvider;
+    }
+}
