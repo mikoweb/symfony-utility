30e87af53d75d8c11d083ae994f4f9b4000d2536
Rafał Mikołajun <rafal@mikoweb.pl>
2016-08-01 23:57:29 +0200

    new methods

--- Entity/Provider/ImagesProviderInterface.php
+++ Entity/Provider/ImagesProviderInterface.php
@@ -19,6 +19,8 @@ namespace vSymfo\Core\Entity\Provider;
  */
 interface ImagesProviderInterface
 {
+    const DEFAULT_PATH = '/default_image.png';
+
     /**
      * Gets the public path for the image associated with the object.
      *
@@ -28,6 +30,16 @@ interface ImagesProviderInterface
      * @return string The public asset path.
      */
     public function asset($obj, $fieldName);
+
+    /**
+     * Gets the same thing asset() or default path.
+     *
+     * @param object $obj       The object.
+     * @param string $fieldName The field name.
+     *
+     * @return string The public asset path or default path.
+     */
+    public function assetOrDefault($obj, $fieldName);
 
     /**
      * Gets the html tags for the image associated with the object.
@@ -52,4 +64,24 @@ interface ImagesProviderInterface
      * @return string A public path which takes into account the base path and URL path
      */
     public function getUrl($path, $packageName = null);
+
+    /**
+     * Gets default path.
+     *
+     * @return string
+     */
+    public function getDefaultPath();
+
+    /**
+     * Gets filtered path for rendering in the browser.
+     * It could be the cached one or an url of filter action.
+     *
+     * @param string $path          The path where the resolved file is expected.
+     * @param string $filter
+     * @param array  $runtimeConfig
+     * @param string $resolver
+     *
+     * @return string
+     */
+    public function imageFilter($path, $filter, array $runtimeConfig = [], $resolver = null);
 }
